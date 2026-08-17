c07aca0ad53dca4d25c916e8b7ab8e6c2c4fe183
Nicollas <nicollaspao1@gmail.com>
2026-08-17 14:08:12 -0300

    começo de tudo(tem nada)

---
+++ src/main/java/com/github/nicsharava/assistente_estudos_matematica/AssistenteEstudosMatematicaApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicsharava.assistente_estudos_matematica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteEstudosMatematicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteEstudosMatematicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicsharava/assistente_estudos_matematica/AssistenteEstudosMatematicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicsharava.assistente_estudos_matematica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteEstudosMatematicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
